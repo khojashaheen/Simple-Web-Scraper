bdb8a2fd8a507362246bfb6a50143941fb40c718
Shaheen <Shaheen.Khoja@iicanada.net>
2023-12-08 21:45:36 -0500

    Updated to return response in JSON Format

--- Main.py
+++ Main.py
@@ -1,6 +1,6 @@
 import requests, re
 from bs4 import BeautifulSoup
-
+import json
 
 def getURL(url):
     response = requests.get(url)
@@ -11,29 +11,37 @@ def scrapeWikiTableFromHTML(html_page):
     table_body = soup.find('tbody')
     rows = table_body.find_all('tr')
     data = []
-    sub_data=[]
     index = 0
     for row in rows:
         cols = row.find_all('td')
         for col in cols:
             if (index==0):
-                sub_data=[]
+                data_dict={}
                 element = col.text.strip()
                 element_clean = re.sub('((\[|\()[a-zA-Z0-9\s]*(\]|\)))', '', element)
-                sub_data.append(element_clean)
+                data_dict["Country"]=element_clean
 
             if (index==4):
                 element = col.text.strip()
                 element_clean = element.replace(",", "")
                 if (element_clean.isnumeric()):
                     element_clean = int(element_clean)
-                sub_data.append(element_clean)
-                data.append(sub_data)
+                data_dict["Population"]=element_clean
+                data.append(data_dict)
             index=(index+1)%6
     return data
-wiki_url = "https://en.wikipedia.org/wiki/List_of_countries_by_population_(United_Nations)"
 
-page = getURL(wiki_url)
-table = scrapeWikiTableFromHTML(page)
+def runWebScraper(url):
+    page = getURL(wiki_url)
+    return scrapeWikiTableFromHTML(page)
+
+wiki_url ="https://en.wikipedia.org/wiki/List_of_countries_by_population_(United_Nations)"
+table = runWebScraper(wiki_url)
+
+
+print("-----------------------------------")
+print("-----------------------------------")
+
+print(json.dumps(table))
 
 
